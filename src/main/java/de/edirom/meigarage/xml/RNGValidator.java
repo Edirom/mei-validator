152ec13338eb581a523bf9af79328783a7fa47ae
Anne Ferger <anne.ferger@uni-paderborn.de>
2022-08-10 10:53:08 +0200

    enable validation for all MEI versions and customizations in 4.0.1 using gihtub rngs

--- src/main/java/de/edirom/meigarage/xml/RNGValidator.java
+++ src/main/java/de/edirom/meigarage/xml/RNGValidator.java
@@ -7,6 +7,13 @@ import org.xml.sax.SAXException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 public class RNGValidator implements XmlValidator {
 
@@ -15,13 +22,44 @@ public class RNGValidator implements XmlValidator {
     private final String schemeUrl;
 
     public RNGValidator(String schemeUrl) {
-        if(schemeUrl == null){
+        if (schemeUrl == null) {
             throw new IllegalArgumentException();
         }
         this.schemeUrl = schemeUrl;
     }
 
     public void validateXml(InputStream inputData, ErrorHandler errorHandler) throws SAXException, FileNotFoundException, IOException, Exception {
+
+        try {
+            // Specify you want a factory for RELAX NG
+            System.setProperty(SchemaFactory.class.getName() + ":" + XMLConstants.RELAXNG_NS_URI, "com.thaiopensource.relaxng.jaxp.XMLSyntaxSchemaFactory");
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.RELAXNG_NS_URI);
+            LOGGER.debug("Uses xml constant : " + XMLConstants.RELAXNG_NS_URI);
+            URL schemaURL = new URL(schemeUrl);
+            //try to download schema by external URL
+            InputStream urlStream = null;
+            try{
+                urlStream = schemaURL.openStream();
+            }catch(IOException ex){
+                    throw ex;
+            }
+            LOGGER.debug("Uses schema url : " + schemeUrl);
+            StreamSource sss = new StreamSource(urlStream);
+            LOGGER.debug("Uses schema source : " + sss);
+            LOGGER.debug(schemaURL);
+            // Load the specific schema you want.
+            // Compile the schema.
+            Schema schema = factory.newSchema(sss);
+            // Get a validator from the schema.
+            Validator validator = schema.newValidator();
+            validator.setErrorHandler(errorHandler);
+            LOGGER.debug("Uses validator : " + validator);
+            StreamSource ssi = new StreamSource(inputData);
+            validator.validate(ssi);
+            LOGGER.info("Inputfile is valid.");
+        } catch (SAXException ex) {
+            throw new SAXException(ex);
+        }
 
     }
 }
